88a671816738f01984e2d3a2dfea73f6b262bd71
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-04-20 18:23:47 +0200

    CNN is now an abstractmodel class

--- src/models/basic_cnn.py
+++ src/models/basic_cnn.py
@@ -2,25 +2,41 @@ from __future__ import division
 import tensorflow as tf
 from src.import_data import get_dataset
 import numpy as np
+from src.answers import AbstractModel
 
-def get_model():
+class BasicCNN(AbstractModel):
 
-    features, labels = get_dataset(100000)
-    x_train, y_train = np.array(features[:-1000]), np.array(labels[:-1000])
-    x_test, y_test = np.array(features[-1000:]), np.array(labels[-1000:])
+    def get_model(self):
+        features, labels = get_dataset(100000)
+        x_train, y_train = np.array(features), np.array(labels)
 
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+        x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(24, 24)),
-        tf.keras.layers.Dense(512, activation=tf.nn.relu),
-        tf.keras.layers.Dense(3, activation=tf.nn.softmax)
-    ])
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(24, 24)),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(3, activation=tf.nn.softmax)
+        ])
 
-    model.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
+        model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=5)
+        model.fit(x_train, y_train, epochs=5)
 
-    return model, x_test, y_test
+        return model
+
+    def evaluate_model(self, data_test):
+        model = self.get_model()
+        answers = model.predict(data_test)
+        answers_final = []
+        for i in answers:
+            new_answer = [i, 0, 0, 0, 0]
+            answers_final.append(new_answer)
+
+        return answers_final
+
+if __name__ == "__main__":
+    b = BasicCNN()
+    b.get_model()
+
