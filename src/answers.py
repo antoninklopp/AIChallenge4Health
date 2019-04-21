448bce36869b3a1163130620612d5a9f6960d2f0
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-04-21 09:48:44 +0200

    Commented file with numpy like comments

--- src/answers.py
+++ src/answers.py
@@ -13,6 +13,16 @@ class AbstractModel(ABC):
 
     @abstractmethod
     def get_model(self):
+        """
+        Should return a model like a tensorflow model 
+        or any machine learning that can evaluate the test 
+        database
+
+        Returns
+        -------
+        Model   model that can be evaluated in the @evaluate_model function
+            any type
+        """
         pass
 
     @abstractmethod
@@ -20,12 +30,23 @@ class AbstractModel(ABC):
         """
         The evaluate should return the answers as follows : 
         classification, spot_1_x, spot_1_y, spot_2_x, spot_2_y
+
+        Parameters
+        ----------
+        data_test       list of np.ndarray
+            The data to test the model on
+
+        Returns
+        -------
+        answers     List of List 
+            a List of answers that should have the following format :
+            [classification, spot_1_x, spot_1_y, spot_2_x, spot_2_y]
         """
         pass
 
     def write_answers_to_csv(self):
         """
-        Writes the answers to a csv file
+        Writes the answers to a csv file : output/answers_{model_name}.csv
         """
         data_test = get_data_test()
         answers_model = self.evaluate_model(data_test)
