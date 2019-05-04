c5dd75fba07c3344c40ce3dd6a26804633200a29
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-05-04 19:13:29 +0200

    Linked darknet results with classification

--- src/read_results_yolo.py
+++ src/read_results_yolo.py
@@ -1,5 +1,5 @@
-from image import Image
-from import_data import RESIZE_FACTOR
+from src.image import Image
+from src.import_data import RESIZE_FACTOR
 
 def read_in():
     """
@@ -8,7 +8,7 @@ def read_in():
     list_images_out = []
     index = 0
     current_image_points = []
-    with open("yolo_results.txt") as results:
+    with open("../darknet/output_global.txt") as results:
         lines = results.readlines()
         for line in lines:
             try:
@@ -20,25 +20,31 @@ def read_in():
                 if not current_image_points:
                     list_images_out.append(Image(index, 0, 0, 0, 0, 0))
                 elif len(current_image_points) == 1:
-                    spot = ((current_image_points[0][1] + current_image_points[0][2])/float(RESIZE_FACTOR), \
-                        (current_image_points[0][3] + current_image_points[0][4])/float(RESIZE_FACTOR))
+                    spot = ((current_image_points[0][1] + current_image_points[0][2])/float(2 * RESIZE_FACTOR), \
+                        (current_image_points[0][3] + current_image_points[0][4])/float(2 * RESIZE_FACTOR))
                     list_images_out.append(Image(index, 1, spot[0], spot[1], 0, 0))
                 else:
                     # Reverse sort the mostr likely images
-                    sort(current_image_points, lambda x: -x[0])
-                    spot1 = ((current_image_points[0][1] + current_image_points[0][2])/float(RESIZE_FACTOR), \
-                        (current_image_points[0][3] + current_image_points[0][4])/float(RESIZE_FACTOR))
-                    spot2 = ((current_image_points[1][1] + current_image_points[1][2])/float(RESIZE_FACTOR), \
-                        (current_image_points[1][3] + current_image_points[1][4])/float(RESIZE_FACTOR))
+                    current_image_points.sort(key=lambda x: -x[0])
+                    spot1 = ((current_image_points[0][1] + current_image_points[0][2])/float(2 * RESIZE_FACTOR), \
+                        (current_image_points[0][3] + current_image_points[0][4])/float(2 * RESIZE_FACTOR))
+                    spot2 = ((current_image_points[1][1] + current_image_points[1][2])/float(2 * RESIZE_FACTOR), \
+                        (current_image_points[1][3] + current_image_points[1][4])/float(2 * RESIZE_FACTOR))
                     list_images_out.append(Image(index, 2, spot1[0], spot1[1], spot2[0], spot2[1]))
 
+                # Reset the current image
+                current_image_points = []
                 index += 1
             except:
                 # Real line
-                percentage, left, right, top, bottom = line.split(" ")
-                percentage, left, right, top, bottom = float(percentage), int(left), int(right), int(top), int(bottom)
+                left, top, right, bottom = line.split(" ")
+                left, right, top, bottom = int(left), int(right), int(top), int(bottom)
+                current_image_points.append([0, left, right, bottom, top])
+
+    return list_images_out
                 
                 
 
 if __name__ == "__main__":
-    read_in()
+    for image in read_in():
+        print(image)
--- tests/test_accuracy_classification.py
+++ tests/test_accuracy_classification.py
@@ -1,9 +1,10 @@
 from src.import_data import get_csv_training, get_dataset, get_dataset_classification_only
 from src.models.image_cnn import ImageCNN
 from src.models.basic_cnn import BasicCNN
-from src.calculate_metrics import calculate_metric_classification
+from src.calculate_metrics import calculate_metric_classification, calculate_metric
 import numpy as np
 from src.image import Image
+from src.read_results_yolo import read_in
 
 def classification_score():
     """
@@ -18,7 +19,16 @@ def classification_score():
     labels = [Image(i, j, 0, 0, 0, 0) for i, j in enumerate(labels)]
     metric = calculate_metric_classification(evaluation, labels)
     return metric
+
+def classification_precision():
+    """
+    Test the classification precision from darknet YOLO network. 
+    """
+    images_yolo = read_in()
+    data_training, labels = get_dataset(len(images_yolo))
+    metric = calculate_metric(images_yolo, labels)
+    return metric
     
 
 if __name__ == "__main__":
-    print(classification_score())
+    print(classification_precision())
